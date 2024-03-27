7d86f121a91556cc35fbd9c6834d19fc2645eebe
alPumpur <aleftinapumpur@gmail.com>
2024-03-27 23:27:13 +0700

    оформление css и html

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -6,10 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pop it MVC</title>
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
 <header>
-    <nav>
+    <nav class="nav">
         <a href="<?= app()->route->getUrl('/hello') ?>">Главная</a>
         <?php
         if (!app()->auth::check()):
@@ -22,7 +23,6 @@
             <a href="<?= app()->route->getUrl('/doctor') ?>">Врачи</a>
             <a href="<?= app()->route->getUrl('/patient') ?>">Пациенты</a>
             <a href="<?= app()->route->getUrl('/visit') ?>">Записи к врачам</a>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Профиль</a>
             <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
         <?php
         endif;
