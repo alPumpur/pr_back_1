baab9d809110703e011adb9349e5fda5c87d01a2
alPumpur <aleftinapumpur@gmail.com>
2024-04-02 10:04:38 +0700

    отображение имен врачей и пациентов

--- views/site/doctor.php
+++ views/site/doctor.php
@@ -41,7 +41,7 @@
             echo '<p>Должность: ' . $doctor->post . '</p>';
             echo '<p>Специализация: ' . $doctor->specialization . '</p>';
             echo '<p style="font-size: 24px; margin-bottom: 0; margin-top: 0">
-                  <img style="max-width: 90px; height: 60px" src="/public/image/' .  $doctor->image  . '" alt="1"></p>';
+                  <img style="max-width: 90px; height: 60px" src="/test/public/image/' .  $doctor->image  . '" alt="1"></p>';
             echo '</div>' ;
         }
         ?>
